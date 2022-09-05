227355e532d568d1abda14a08e1f9802084cbf39
MariamMasri <meremmasre@gmail.com>
2022-09-05 07:11:04 +0300

    add lap class-11

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import axios from 'axios';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state =
+      { bookArr: [] }
+  } componentDidMount = () => {
+    axios.get(`http://localhost:3010/getBook`).then(result => {
+      console.log(result.data);
+      this.setState({ bookArr: result.data })
+    }).catch(err => { console.log(err); })
+  }
+  render() {
+    return (
+      <div>
+        <h1>Book System</h1>
+        {this.state.bookArr.map(item => {
+          return (<div>
+            <h3> title : {item.title} </h3>
+            <p> description : {item.description}</p>
+            <p> status : {item.status}</p>
+          </div>)
+        })}       </div>)
+  }
+}
+export default App;
